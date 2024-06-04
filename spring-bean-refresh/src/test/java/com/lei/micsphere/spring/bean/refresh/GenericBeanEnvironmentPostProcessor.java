e1cef49e3c9081ca6d669866a897165aa1a6b3e9
lei <17674030991@163.com>
2024-06-04 21:51:25 +0800

    [spring-bean-refresh]: RefreshEvent事件

---
+++ spring-bean-refresh/src/test/java/com/lei/micsphere/spring/bean/refresh/GenericBeanEnvironmentPostProcessor.java
@@ -0,0 +1,41 @@
+package com.lei.micsphere.spring.bean.refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.io.support.ResourcePropertySource;
+
+import java.io.IOException;
+
+/**
+ * <p>
+ * ClasspathEnvironmentPostProcessor    <br/>
+ * 这里只是演示效果 <br/>
+ * 直接固定从某个文件下获取，这样当RefreshEvent刷新后，此后置处理器又会重新被调用，然后重新添加到environment中，
+ * 对原来的数据进行覆盖
+ * </p>
+ *
+ * @author 伍磊
+ */
+public class GenericBeanEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
+    private String filePath = "file:/Users/wulei/IdeaProjects/personal/spring-refresh/spring-bean-refresh/src/test/java/com/lei/micsphere/spring/bean/refresh/genericBean.properties";
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        try {
+            ResourcePropertySource resourcePropertySource = new ResourcePropertySource(
+                    "genericBean", filePath, this.getClass().getClassLoader());
+            MutablePropertySources propertySources = environment.getPropertySources();
+            propertySources.addLast(resourcePropertySource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
---
+++ spring-bean-refresh/src/test/java/com/lei/micsphere/spring/bean/refresh/GenericBeanRefreshTest.java
@@ -0,0 +1,124 @@
+package com.lei.micsphere.spring.bean.refresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.endpoint.event.RefreshEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * <p>
+ * 普通的 pojo bean 刷新
+ * </p>
+ *
+ * @author 伍磊
+ */
+@SpringBootTest
+public class GenericBeanRefreshTest {
+
+    @Autowired
+    private GenericBeanRefreshTestConfiguration genericBeanRefreshTestConfiguration;
+
+    @Autowired
+    private User user;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @Test
+    public void test() throws InterruptedException {
+        assertNotNull(user);
+        assertEquals(user.username, "xx");
+        assertEquals(user.age, 10);
+        assertEquals(genericBeanRefreshTestConfiguration.getUsername(), "xx");
+
+        // 手动修改 genericBean.properties
+        // 需要手动保存下
+        // xx -> yy
+        System.out.println("修改classpath:/genericBean.properties配置");
+
+        // 发布refresh事件
+        RefreshEvent refreshEvent = new RefreshEvent(this, null, null);
+        applicationContext.publishEvent(refreshEvent);
+
+        // refresh事件导致环境变量的刷新 org.springframework.cloud.context.refresh.ContextRefresher.refreshEnvironment
+        // 从而 @Value 修饰的数据会变化
+        assertEquals(genericBeanRefreshTestConfiguration.getUsername(), "yy");
+
+        // 但是非@Value修饰的不会变化，因为没有事件进行处理
+        // 需要主动消费 EnvironmentChangeEvent 事件
+        assertEquals(user.username, "xx");
+
+    }
+
+
+    @Configuration
+    @RefreshScope
+    @ImportAutoConfiguration(classes = {RefreshAutoConfiguration.class})
+    public static class GenericBeanRefreshTestConfiguration {
+
+        @Value("${test.user.name}")
+        private String username;
+
+        @Value("${test.user.age}")
+        private Integer age;
+
+        @Bean
+        public User user() {
+            User user = new User();
+            user.setUsername(username);
+            user.setAge(age);
+            return user;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+
+    public static class User {
+        private String username;
+        private Integer age;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
